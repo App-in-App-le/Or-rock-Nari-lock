d30d76b169ae7294fd49e961d6853758107862cb
chopmozzi <44396392+chopmozzi@users.noreply.github.com>
2024-02-13 21:12:26 +0900

    :sparkles: Xcode 프로젝트 생성

---
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Or-rock-Nari-lock
+//
+//  Created by 황지웅 on 2/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
