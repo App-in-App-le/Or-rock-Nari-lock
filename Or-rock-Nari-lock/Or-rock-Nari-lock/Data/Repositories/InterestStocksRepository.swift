3d46a836310cab4bc60cbf1ec8b8d0dcebb7c5a2
chopmozzi <chopmojji@gmail.com>
2024-02-28 23:38:02 +0900

    :sparkles: Set StockRepository

---
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/Data/Repositories/InterestStocksRepository.swift
@@ -0,0 +1,18 @@
+//
+//  InterestStocksRepository.swift
+//  Or-rock-Nari-lock
+//
+//  Created by 황지웅 on 2/28/24.
+//
+
+import Foundation
+
+final class DefaultInterestStocksRepository: InterestStocksRepository {
+    
+}
+
+extension DefaultInterestStocksRepository {
+    func loadInterestStockList() {
+        <#code#>
+    }
+}
--- Or-rock-Nari-lock/Or-rock-Nari-lock/Domain/Interfaces/Repository/InterestStocksRepository.swift
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/Domain/Interfaces/Repository/InterestStocksRepository.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 protocol InterestStocksRepository {
-    
+    func loadInterestStockList()
 }
