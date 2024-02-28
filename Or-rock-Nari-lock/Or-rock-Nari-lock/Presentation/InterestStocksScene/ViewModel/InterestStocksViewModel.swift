2590d46621871f0d37efe9d593d2b57f901915f2
chopmozzi <chopmojji@gmail.com>
2024-02-28 17:57:12 +0900

    :sparkles: InterestStocksViewModel 파일 생성

---
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/Presentation/InterestStocksScene/ViewModel/InterestStocksViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  InterestStocksViewModel.swift
+//  Or-rock-Nari-lock
+//
+//  Created by 황지웅 on 2/28/24.
+//
+
+import Foundation
+
+protocol InterestStocksViewModelInput {
+    
+}
+
+protocol InterestStocksViewModelOutput {
+
+}
+
+typealias InterestStocksViewModel = InterestStocksViewModelInput & InterestStocksViewModelOutput
+
+
