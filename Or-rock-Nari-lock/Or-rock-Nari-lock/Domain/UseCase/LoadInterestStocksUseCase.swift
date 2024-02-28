da35db573ef79278f01650f56e6af989ce5549b4
chopmozzi <chopmojji@gmail.com>
2024-02-28 18:30:07 +0900

    :sparkles: create LoadInterestStocksUseCase

---
+++ Or-rock-Nari-lock/Or-rock-Nari-lock/Domain/UseCase/LoadInterestStocksUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  LoadInterestStocksUseCase.swift
+//  Or-rock-Nari-lock
+//
+//  Created by 황지웅 on 2/28/24.
+//
+
+import Foundation
+
+protocol LoadInterestStocksUseCase {
+
+}
+
+final class DefaultLoadInterestStocksUseCase: LoadInterestStocksUseCase {
+
+}
